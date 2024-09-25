dbfc39af145efc35ec5c5871b41280fe2b72d532
git-psj <20211485@daejin.ac.kr>
2024-09-25 23:04:06 +0900

    Update diary.py

--- diary.py
+++ diary.py
@@ -2,10 +2,24 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
-# Firebase 초기화
-firebase_credentials = st.secrets['firebase']['firebase_credentials']
-cred = credentials.Certificate(firebase_credentials)
-firebase_admin.initialize_app(cred)
+# secrets.toml에서 Firebase 자격 증명 정보 가져오기
+firebase_credentials = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+}
+
+# 파싱된 dict로 Firebase 초기화
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_credentials)
+    firebase_admin.initialize_app(cred)
 
 # Firestore 초기화
 db = firestore.client()
