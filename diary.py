6da49c2d5c7c3b8c2e60a38b0b4224cb8ccd6381
git-psj <20211485@daejin.ac.kr>
2024-09-26 13:16:00 +0900

    Update diary.py

--- diary.py
+++ diary.py
@@ -77,7 +77,7 @@ def login_page():
             # 로그인 성공 시 처리 로직 추가
         
             st.success(f"{email}님, 로그인 성공!")
-        except auth.exceptions.FirebaseError as e:
+        except firebase_admin.exceptions.FirebaseError as e:
             st.error(f"로그인 실패: {str(e)}")
 
 # 기본 페이지 설정
